9ffcd8998845c91192131256651615989f2e0889
Arturo Cobos <arturo@co-boss.com>
2023-10-04 09:10:40 -0500

    initial load
    initial load

---
+++ 09 Conceptual & logical data alignment/Building a Data Dictionary & realizing Concepts (Published)/data/slide9.js
@@ -0,0 +1,17 @@
+(function(){var loadHandler=window['sl_{9BC5336C-C307-4772-8996-5122680B65D8}'];loadHandler&&loadHandler(8, '<div id="spr0_51fe1bc"><div id="spr1_51fe1bc" class="kern slide"><img id="img9_51fe1bc" src="data/img3.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_51fe1bc" style="left:890.702px;top:476.479px;"><img id="img0_51fe1bc" src="data/img0.png" width="42.5" height="43" alt="" style="left:0.056px;top:-0.031px;"/></div><div id="spr4_51fe1bc" style="left:22.302px;top:509.457px;"><div style="width:0px;"><span id="txt0_51fe1bc" class="nokern" style="left:16.946px;top:-0.178px;">9</span></div></div><div id="svg0_51fe1bc" style="left:7.429px;top:-1.486px;"><svg width="68" height="11" viewBox="-1.486 -1.486 68 11"><path fill="#31b672" d="M0,1.486 A1.486,1.486 0 0,1 1.486,0 H62.886 A1.486,1.486 0 0,1 64.372,1.486 V8.914 A0,0 0 0,1 64.372,8.914 H0 A0,0 0 0,1 0,8.914 Z"/></svg></div><div id="spr5_51fe1bc" style="left:-0px;top:505.602px;"><div style="width:0px;"><span id="txt1_51fe1bc" class="nokern" data-width="19.256836" style="left:446.083px;top:4.686px;">C2 -</span></div><div style="width:0px;"><span id="txt2_51fe1bc" class="nokern" data-width="45.369141" style="left:468.583px;top:4.686px;">Restricted</span></div></div></div><div id="spr2_51fe1bc" class="kern slide"><div id="spr6_51fe1bc" style="left:153.39px;top:117.988px;"><img id="img1_51fe1bc" src="data/img19.png" width="374" height="364" alt="Graphical user interface, application\
+\
+Description automatically generated with medium confidence" style="left:-0.035px;top:-0.011px;"/></div><div id="spr7_51fe1bc" style="left:519.981px;top:128.039px;"><img id="img2_51fe1bc" src="data/img20.png" width="345" height="354.5" alt="Text\
+\
+Description automatically generated" style="left:-0.058px;top:0.039px;"/></div><div id="spr8_51fe1bc" style="left:195.154px;top:313.861px;"><img id="img3_51fe1bc" src="data/img21.png" width="148.5" height="96" alt="A picture containing table\
+\
+Description automatically generated" style="left:0.049px;top:-0.059px;"/></div><div id="spr9_51fe1bc" style="left:273.293px;top:267.577px;"><img id="img4_51fe1bc" src="data/img21.png" width="148.5" height="96" alt="A picture containing table\
+\
+Description automatically generated" style="left:0.049px;top:-0.059px;"/></div><div id="spr10_51fe1bc" style="left:226.878px;top:257.091px;"><img id="img5_51fe1bc" src="data/img21.png" width="148.5" height="96" alt="A picture containing table\
+\
+Description automatically generated" style="left:0.049px;top:-0.059px;"/></div><div id="spr11_51fe1bc" style="left:582.582px;top:303.375px;"><img id="img6_51fe1bc" src="data/img21.png" width="148.5" height="96" alt="A picture containing table\
+\
+Description automatically generated" style="left:0.049px;top:-0.059px;"/></div><div id="spr12_51fe1bc" style="left:660.721px;top:257.091px;"><img id="img7_51fe1bc" src="data/img21.png" width="148.5" height="96" alt="A picture containing table\
+\
+Description automatically generated" style="left:0.049px;top:-0.059px;"/></div><div id="spr13_51fe1bc" style="left:614.306px;top:246.605px;"><img id="img8_51fe1bc" src="data/img21.png" width="148.5" height="96" alt="A picture containing table\
+\
+Description automatically generated" style="left:0.049px;top:-0.059px;"/></div></div></div>', '{"s":[]}');})();
---
+++ 10 Building a Database through synchronization/Building a Database through synchronization (Published)/data/slide7.js
@@ -0,0 +1,15 @@
+(function(){var loadHandler=window['sl_{C75DD1CC-1218-48F2-BC82-82D53D72B146}'];loadHandler&&loadHandler(6, '<div id="spr0_51ae203"><div id="spr1_51ae203" class="kern slide"><img id="img8_51ae203" src="data/img3.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_51ae203" style="left:890.702px;top:476.479px;"><img id="img0_51ae203" src="data/img0.png" width="42.5" height="43" alt="" style="left:0.056px;top:-0.031px;"/></div><div id="spr4_51ae203" style="left:22.302px;top:509.457px;"><div style="width:0px;"><span id="txt0_51ae203" class="nokern" style="left:16.946px;top:-0.178px;">7</span></div></div><div id="svg0_51ae203" style="left:7.429px;top:-1.486px;"><svg width="68" height="11" viewBox="-1.486 -1.486 68 11"><path fill="#31b672" d="M0,1.486 A1.486,1.486 0 0,1 1.486,0 H62.886 A1.486,1.486 0 0,1 64.372,1.486 V8.914 A0,0 0 0,1 64.372,8.914 H0 A0,0 0 0,1 0,8.914 Z"/></svg></div><div id="spr5_51ae203" style="left:-0px;top:505.602px;"><div style="width:0px;"><span id="txt1_51ae203" class="nokern" data-width="19.256836" style="left:446.083px;top:4.686px;">C2 -</span></div><div style="width:0px;"><span id="txt2_51ae203" class="nokern" data-width="45.369141" style="left:468.583px;top:4.686px;">Restricted</span></div></div></div><div id="spr2_51ae203" class="kern slide"><div id="spr6_51ae203" style="left:60.308px;top:88.5px;"><img id="img1_51ae203" src="data/img19.png" width="844" height="363" alt="Text\
+\
+Description automatically generated"/></div><div id="spr7_51ae203" style="left:133.442px;top:266.159px;"><img id="img2_51ae203" src="data/img20.png" width="152" height="79.5" alt="Table\
+\
+Description automatically generated with medium confidence" style="top:-0.011px;"/></div><div id="spr8_51ae203" style="left:237.692px;top:230.273px;"><img id="img3_51ae203" src="data/img20.png" width="152" height="79.5" alt="Table\
+\
+Description automatically generated with medium confidence" style="top:-0.011px;"/></div><div id="spr9_51ae203" style="left:180.942px;top:305.364px;"><img id="img4_51ae203" src="data/img20.png" width="152" height="79.5" alt="Table\
+\
+Description automatically generated with medium confidence" style="top:-0.011px;"/></div><div id="spr10_51ae203" style="left:581.175px;top:266.159px;"><img id="img5_51ae203" src="data/img20.png" width="152" height="79.5" alt="Table\
+\
+Description automatically generated with medium confidence" style="top:-0.011px;"/></div><div id="spr11_51ae203" style="left:685.425px;top:230.273px;"><img id="img6_51ae203" src="data/img20.png" width="152" height="79.5" alt="Table\
+\
+Description automatically generated with medium confidence" style="top:-0.011px;"/></div><div id="spr12_51ae203" style="left:628.675px;top:305.364px;"><img id="img7_51ae203" src="data/img20.png" width="152" height="79.5" alt="Table\
+\
+Description automatically generated with medium confidence" style="top:-0.011px;"/></div></div></div>', '{"s":[]}');})();
